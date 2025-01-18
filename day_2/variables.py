8e64fa1f2cc44660ac6ba96e7f118cda47dfbf57
H-Mubashir <155391246+H-Mubashir@users.noreply.github.com>
2025-01-18 10:50:09 +0100

    Update variables.py

--- day_2/variables.py
+++ day_2/variables.py
@@ -1,8 +1,8 @@
 """Day 2: 30 Days of python programming"""
 
-first_name = "Mubashir"
-last_name = "Hamzah"
-full_name = "Hamzah Mubashir Sharafdeen"
+f_name = "Mubashir"
+l_name = "Hamzah"
+full_name = f_name + l_name
 country = "Nigeria"
 city = "Ilorin"
 age = 22
@@ -11,3 +11,50 @@ is_married = "Single"
 is_true = "Yes"
 is_light = "No"
 name, age, city = "Mubashir", 22, "Ilorin"
+
+print("Exercises: Level 2/n")
+type(f_name)
+type(l_name)
+type(full_name)
+type(country)
+type(city)
+type(age)
+type(year)
+type(is_married)
+type(is_single)
+type(is_light)
+type(name)
+
+len(f_name)
+len(l_name)
+num_one = 5
+num_two = 4
+total = num_one + num_two
+var_diff = num_two - num_one
+var_product = num_one * num_two
+var_division =num_one / num_two
+var_remainder = num_one % num_two
+var_exp = num_one**num_two
+floor_division = num_one// num_two
+"""
+  The radius of a circle is 30 meters.
+  Calculate the area of a circle and assign the value to a variable name of 
+  area_of_circle
+  Calculate the circumference of a circle and assign the value to a variable name    of circum_of_circle
+  Take radius as user input and calculate the area.
+"""
+radius = 30
+pi = 3.14
+area = pi*radiu**2
+circum_of_circle = 2* pi *radius
+print(f"The area of the circle is {area}, and the circumfrence is {circum_of_circle }")
+
+user_radius = float(input("Enter your radius"))
+user_area = pi*user_radius **2
+
+first_name = input("Enter your Name")
+last_name = input("Enter your Name")
+country = input("Enter your Name")
+age = int(input("Enter your Name"))
+ help('keywords')
+
