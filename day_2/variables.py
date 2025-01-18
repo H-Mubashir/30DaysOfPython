09f1b5dd7db87b6e44e2056eeddc66e588b16131
H-Mubashir <155391246+H-Mubashir@users.noreply.github.com>
2025-01-18 10:02:39 +0100

    Update variables.py

--- day_2/variables.py
+++ day_2/variables.py
@@ -1,1 +1,13 @@
+"""Day 2: 30 Days of python programming"""
 
+first_name = "Mubashir"
+last_name = "Hamzah"
+full_name = "Hamzah Mubashir Sharafdeen"
+country = "Nigeria"
+city = "Ilorin"
+age = 22
+year = 2025
+is_married = "Single"
+is_true = "Yes"
+is_light = "No"
+name, age, city = "Mubashir", 22, "Ilorin"
